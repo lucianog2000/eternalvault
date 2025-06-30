f8f67975d841d9d7328edd459fd1d6c021ad2655
Luchex <40703841+lucianog2000@users.noreply.github.com>
2025-06-30 11:16:15 -0300

    Updated dictionary.js

--- src/lingo/dictionary.js
+++ src/lingo/dictionary.js
@@ -506,6 +506,7 @@ export default {
         "5/0/init/body/13/argument/1/3/27/3": {
           content: {
             en: "Create Access Key",
+            fr: "Créer une clé d'accès",
           },
           hash: "65ace5e9d85d8ad2b67b17e8b931d535",
         },
